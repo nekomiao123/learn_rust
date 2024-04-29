1d3feb8fb943b53cea7beb43f33ccd6d69369bc2
nekokiku <447140443@qq.com>
2024-04-29 21:27:36 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+fn sum(x: i32, y: i32)-> i32 {
+    x + y
+}
+
+fn hello() {
+    println!("Hello, world!");
+}
+
 fn main() {
     let (x, y) = (1, 2);
     let s = sum(x, y);
 
     assert_eq!(s, 3);
-    println!("{}",s)
-}
-
-fn sum(x: i32, y: i32)-> i32 {
-    x + y
+    println!("The value is {}",s);
+    hello();
 }
 
