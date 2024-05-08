0eeebc0ffb76ad4edfa242a4af19635866a14a65
nekokiku <447140443@qq.com>
2024-05-09 01:40:11 +0800

    borrow

--- session1/variables/src/main.rs
+++ session1/variables/src/main.rs
@@ -9,6 +9,19 @@ fn double_or_nothing(n:i32) -> i32{
     0
 }
 
+fn greet(s: String){
+    println!("Hello {}", s);
+}
+
+fn greet_borrow(s: &String){
+    println!("Hello {}", s);
+}
+
+fn greet_borrow_mut(s: &mut String){
+    // s.push_str(" World");
+    *s = format!("{} World", s);
+}
+
 fn main() {
     let n:i32 = 5;
     let mut m = 2;
@@ -30,4 +43,14 @@ fn main() {
         0
     };
     println!("a is {}", a);
+
+    let name = "Hello".to_string();
+    greet(name.clone());
+    // greet(name);
+    greet_borrow(&name);
+    greet_borrow(&name);
+
+    let mut name2 = "Hello".to_string();
+    greet_borrow_mut(&mut name2);
+    println!("{}", name2);
 }
