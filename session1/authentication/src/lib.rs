8825e8bbc193fce5778e451ef9c40f0820ae3018
nekokiku <447140443@qq.com>
2024-05-09 03:21:53 +0800

    enum and match

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -6,6 +6,42 @@ pub fn greet_user(name: &str) -> String{
     format!("Hello, {}!", name)
 }
 
+
+#[derive(PartialEq, Debug)]
+pub enum LoginAction {
+    Granted(LoginRole),
+    Denied,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum LoginRole {
+    Admin,
+    User,
+}
+
+pub fn login(username: &str, password: &str) -> Option<LoginAction> {
+    let username = username.to_lowercase();
+
+    if username != "admin" && username != "alice" {
+        return None;
+    }
+
+    if username == "admin" && password == "password" {
+        Some(LoginAction::Granted(LoginRole::Admin))
+    } else if username == "alice" && password == "password" {
+        Some(LoginAction::Granted(LoginRole::User))
+    } else {
+        Some(LoginAction::Denied)
+    }
+}
+
+pub fn read_line() -> String{
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -19,6 +55,12 @@ mod tests {
     fn test_greet_user() {
         let result = greet_user("Alice");
         assert_eq!(result, "Hello, Alice!");
+    }
+    #[test]
+    fn test_login() {
+        assert_eq!(login("admin", "password"), Some(LoginAction::Granted(LoginRole::Admin)));
+        assert_eq!(login("alice", "password"), Some(LoginAction::Granted(LoginRole::User)));
+        assert_eq!(login("Alice", "wrong"), Some(LoginAction::Denied));
     }
 }
 
--- session1/login/src/main.rs
+++ session1/login/src/main.rs
@@ -1,8 +1,48 @@
-use authentication::greet_user;
+use authentication::{login, read_line, LoginAction, LoginRole};
+
 
 fn main() {
-    let name = "Alice";
-    println!("{}", greet_user(name));
+    let mut tries = 0;
+    loop{
+        println!("Enter your username:");
+        let username = read_line();
+        println!("Enter your password:");
+        let password = read_line();
+
+        match login(&username, &password) {
+            Some(LoginAction::Granted(role)) => {
+                match role {
+                    LoginRole::Admin => println!("Welcome Admin"),
+                    LoginRole::User => println!("Welcome User"),
+                }
+                break;
+            }
+            Some(LoginAction::Denied) =>{
+                println!("Wrong password\n");
+            }
+            None => {
+                println!("It seems we have new user")
+            }
+        }
+        tries += 1;
+        if tries >= 3 {
+            println!("Too many tries!\n");
+            break;
+        }
+
+        // if login(&username, &password) {
+        //     println!("Welcome!");
+        //     break;
+        // } else {
+        //     println!("Wrong username or password\n");
+        //     tries += 1;
+        //     if tries >= 3 {
+        //         println!("Too many tries!\n");
+        //         break;
+        //     }
+        // }
+        
+    }
 }
 
 
