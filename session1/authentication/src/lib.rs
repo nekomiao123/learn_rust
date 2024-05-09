3840929a73ecf8b7c47ea81d7e71183a03d2fdc1
nekokiku <447140443@qq.com>
2024-05-09 20:44:20 +0800

    struct

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -7,32 +7,65 @@ pub fn greet_user(name: &str) -> String{
 }
 
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginAction {
     Granted(LoginRole),
     Denied,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginRole {
     Admin,
     User,
 }
 
+pub struct User{
+    pub username: String,
+    pub password: String,
+    pub role: LoginRole,
+}
+
+impl User{
+    pub fn new(username: &str, password: &str, role: LoginRole) -> User {
+        User { 
+            username: username.to_lowercase(),
+            password: password.to_string(), 
+            role
+        }
+    }
+}
+
+pub fn get_users() -> [User; 2] {
+    [
+        User::new("admin", "password", LoginRole::Admin),
+        User::new("alice", "password", LoginRole::User),
+    ]
+}
+
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let username = username.to_lowercase();
+    let users = get_users();
+    if let Some(user) = users.iter().find(|user| user.username == username) {
+        if user.password == password {
+            return Some(LoginAction::Granted(user.role.clone()));
+        } else {
+            return Some(LoginAction::Denied);
+        }
+    } 
+    None
 
-    if username != "admin" && username != "alice" {
-        return None;
-    }
+    // let username = username.to_lowercase();
+    // if username != "admin" && username != "alice" {
+    //     return None;
+    // }
 
-    if username == "admin" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::Admin))
-    } else if username == "alice" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::User))
-    } else {
-        Some(LoginAction::Denied)
-    }
+    // if username == "admin" && password == "password" {
+    //     Some(LoginAction::Granted(LoginRole::Admin))
+    // } else if username == "alice" && password == "password" {
+    //     Some(LoginAction::Granted(LoginRole::User))
+    // } else {
+    //     Some(LoginAction::Denied)
+    // }
 }
 
 pub fn read_line() -> String{
